516cac5952672b1a0919a8d54084c886dd183371
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-04-25 20:01:08 +0200

    Change scritpt.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// 8.6 Instrukcje warunkowe w praktyce
+
+var a = prompt('Provide a value'),
+    b = prompt('Provide b value'),
+    value = (a * a) - (2 * a * b) - (b * b);
+
+alert('Your result is ' + value);
+
+
+
+if (value > 0) {
+    console.log('Wartosc dodatnia')
+    }
+    else if (value < 0) {
+    console.log('Wartosc ujemna')
+    }
+    else {
+    console.log('Wartosc równa zero')
+    };
+               
+
