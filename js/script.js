b6b6c5fde0a8acaa3b50945782d0a33006c49490
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-06 20:28:32 +0200

    Correct js file

--- js/script.js
+++ js/script.js
@@ -3,19 +3,14 @@
 var a = prompt('Provide a value'),
     b = prompt('Provide b value'),
     value = (a * a) - (2 * a * b) - (b * b);
-
 alert('Your result is ' + value);
 
-
-
 if (value > 0) {
-    console.log('Wartosc dodatnia')
-    }
-    else if (value < 0) {
-    console.log('Wartosc ujemna')
-    }
-    else {
-    console.log('Wartosc równa zero')
-    };
+    console.log('Wartosc dodatnia');
+} else if (value < 0) {
+    console.log('Wartosc ujemna');
+} else {
+    console.log('Wartosc równa zero');
+}
                
 
